3a35c891022a017bc23f47eaa89de693d2e846ef
lion-101 <65544694+lion-101@users.noreply.github.com>
2020-10-06 15:38:22 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paper=new Paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	launcher = new Launcher(paper.body,{x:200, y:50});
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	//Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  launcher.display();
+ 
+  
+  
+ 
+}
+
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+
+
+
